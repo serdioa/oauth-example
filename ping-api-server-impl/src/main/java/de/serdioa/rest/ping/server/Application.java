d9b1e36154688f84c87bd446952eef81e5997a5a
serdioa <serdioa@t-online.de>
2022-04-17 13:25:28 +0200

    Added Swagger UI

--- ping-api-server-impl/src/main/java/de/serdioa/rest/ping/server/Application.java
+++ ping-api-server-impl/src/main/java/de/serdioa/rest/ping/server/Application.java
@@ -1,8 +1,14 @@
 package de.serdioa.rest.ping.server;
 
+import com.fasterxml.jackson.databind.Module;
+import org.openapitools.jackson.nullable.JsonNullableModule;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -11,5 +17,31 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+
+    @Bean
+    public WebMvcConfigurer webConfigurer() {
+        return new WebMvcConfigurer() {
+            /*@Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("Content-Type");
+            }*/
+
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/swagger-ui/**")
+                        .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/3.14.2/");
+            }
+        };
+    }
+
+
+    @Bean
+    public Module jsonNullableModule() {
+        return new JsonNullableModule();
     }
 }
