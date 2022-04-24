a5882c54af359d02360937d5fdc28f530cfeaabd
serdioa <serdioa@t-online.de>
2022-04-24 11:44:08 +0200

    Moved default configuration file for OAuth implementation server to a standard location

--- authorization-server/start.sh
+++ authorization-server/start.sh
@@ -9,7 +9,12 @@ JAVA_OPTIONS="-XX:+UseShenandoahGC -XX:+UseStringDeduplication"
 
 EXEC_JAR=$(find target -name *.jar)
 
-java ${JAVA_OPTIONS} -jar ${EXEC_JAR} \
-    --spring.config.location=file:src/config/authorization-server.properties \
-    "$@"
+mvn package spring-boot:repackage
+
+JAR=$(/bin/find target -name authorization-server-*.jar)
+if [[ -z "${JAR}" ]] ; then
+    echo "Can not find executable jar" >&2
+else
+    java ${JAVA_OPTIONS} -jar ${JAR} "$@"
+fi
 
