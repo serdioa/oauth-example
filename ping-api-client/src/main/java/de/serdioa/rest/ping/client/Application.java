3d7741634eb8460332754c0540355f8174570749
serdioa <serdioa@t-online.de>
2022-06-05 15:44:37 +0200

    Added sample hard-coded OAuth2 client configuration

--- ping-api-client/src/main/java/de/serdioa/rest/ping/client/Application.java
+++ ping-api-client/src/main/java/de/serdioa/rest/ping/client/Application.java
@@ -4,15 +4,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Optional;
+import java.util.TreeMap;
 
 import de.serdioa.rest.ping.ApiClient;
 import de.serdioa.rest.ping.api.PingApi;
 import de.serdioa.rest.ping.model.Pong;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.InMemoryReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 
@@ -22,13 +37,38 @@ public class Application implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
+    
     private ApiClient apiClient;
     private PingApi pingApi;
 
     @Override
     public void run(String... args) throws Exception {
-        this.apiClient = new ApiClient();
+        ClientRegistration clientRegistration = ClientRegistration
+                .withRegistrationId("test")
+                .tokenUri("http://localhost:8070/oauth2/token")
+                .clientId("aladdin")
+                .clientSecret("sesame")
+                .scope("read", "write")
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .build();
+        
+        ReactiveClientRegistrationRepository clientRegistrationRespository =
+                new InMemoryReactiveClientRegistrationRepository(clientRegistration);
+        
+        InMemoryReactiveOAuth2AuthorizedClientService clientService =
+                new InMemoryReactiveOAuth2AuthorizedClientService(clientRegistrationRespository);
+        
+        AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager clientManager =
+                new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(clientRegistrationRespository, clientService);
+
+        ServerOAuth2AuthorizedClientExchangeFilterFunction oauth = new ServerOAuth2AuthorizedClientExchangeFilterFunction(clientManager);
+        oauth.setDefaultClientRegistrationId("test");
+        
+        WebClient webClient = WebClient.builder()
+                .filter(oauth)
+                .build();
+        
+        this.apiClient = new ApiClient(webClient);
         this.apiClient.setBasePath("http://localhost:8080");
         
         this.pingApi = new PingApi(this.apiClient);
