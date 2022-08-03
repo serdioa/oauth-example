409b5f9ae20cbc5a16ffd87f94740fd0d087d225
serdioa <serdioa@t-online.de>
2022-08-03 11:59:31 +0200

    Added security configuration to Ping API methods

--- ping-api-server-impl/src/main/java/de/serdioa/rest/ping/server/SecurityConfiguration.java
+++ ping-api-server-impl/src/main/java/de/serdioa/rest/ping/server/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package de.serdioa.rest.ping.server;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,13 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests(configurer -> {
-//            configurer.antMatchers("/rest/**")
-//                    .authenticated();
         http.oauth2ResourceServer(configurer -> {
             configurer.jwt();
         });
@@ -25,21 +24,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         http.csrf().disable();
     }
-
-//    public AuthorizationManager<MethodInvocation> myAuthorizationManager() {
-//        return (authenticationSupplier, methodInvocation) -> {
-//            System.out.println("Custom authorization manager");
-//            Authentication auth = authenticationSupplier.get();
-//            System.out.println("    auth=" + auth);
-//            System.out.println("    methodInvocation=" + methodInvocation);
-//
-//            return new AuthorityAuthorizationDecision(true, Collections.emptySet());
-//        };
-//    }
-    // OAuth2ResourceServerConfigurer
-//    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
-//    @Override
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
 }
--- ping-api-server-impl/src/main/java/de/serdioa/rest/ping/server/impl/PingApiImpl.java
+++ ping-api-server-impl/src/main/java/de/serdioa/rest/ping/server/impl/PingApiImpl.java
@@ -1,17 +1,12 @@
 package de.serdioa.rest.ping.server.impl;
 
 import java.time.OffsetDateTime;
-import java.util.Collection;
 import java.util.Optional;
 
 import de.serdioa.rest.ping.api.PingApiDelegate;
 import de.serdioa.rest.ping.model.Pong;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.NativeWebRequest;
 
@@ -36,40 +31,9 @@ public class PingApiImpl implements PingApiDelegate {
 
 
     @Override
+    @PreAuthorize("hasAuthority('read')")
     public ResponseEntity<Pong> restV1PingGet() {
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        System.out
-                .printf("securityContext=%s (%s)%n", securityContext, (securityContext == null ? null : securityContext
-                        .getClass()));
-
-        if (securityContext != null) {
-            Authentication authentication = securityContext.getAuthentication();
-            System.out
-                    .printf("authentication=%s (%s)%n", authentication, (authentication == null ? null : authentication
-                            .getClass()));
-
-            if (authentication != null) {
-                String name = authentication.getName();
-                System.out.printf("name=%s (%s)%n", name, (name == null ? null : name.getClass()));
-
-                Object principal = authentication.getPrincipal();
-                System.out.printf("principal=%s (%s)%n", principal, (principal == null ? null : principal.getClass()));
-
-                if (principal instanceof UserDetails) {
-                    String principalName = ((UserDetails) principal).getUsername();
-                    System.out
-                            .printf("principalName=%s (%s)%n", principalName, (principalName == null ? null : principalName
-                                    .getClass()));
-                }
-
-                Object details = authentication.getDetails();
-                System.out.printf("details=%s (%s)%n", details, (details == null ? null : details.getClass()));
-
-                Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-                System.out.printf("authorities=%s (%s)%n", authorities, (authorities == null ? null : authorities
-                        .getClass()));
-            }
-        }
+        SecurityTestHelper.printAuthDetails();
 
         Pong pong = new Pong();
         pong.setTimestamp(OffsetDateTime.now());
@@ -80,7 +44,10 @@ public class PingApiImpl implements PingApiDelegate {
 
 
     @Override
+    @PreAuthorize("hasAuthority('write')")
     public ResponseEntity<Pong> restV1PingPost(String token) {
+        SecurityTestHelper.printAuthDetails();
+
         Pong pong = new Pong();
         pong.setTimestamp(OffsetDateTime.now());
         pong.setToken(token != null ? token : DEFAULT_TOKEN);
